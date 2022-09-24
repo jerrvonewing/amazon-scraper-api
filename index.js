500742df1524e2968a176b2e803b428b221221c4
jerrvonewing <jerrvonewing.cs@gmail.com>
2022-09-24 01:10:42 -0500

    Getting results by search term

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
 // Dependencies
 const express = require('express');
-const request = require('request-promise');
+const axios = require('axios')
+const cheerio = require('cheerio');
+const request = require('request-promise')
 
 // Initialize express application
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Allow application to parse json
-app.use(express.json());
+const results = [];
+// Price 
+
+// Rating
+
+// Description
+
+// URL
+
+
+// Name
+
+
 
 // Initialize API base
 app.get('/', (req, res) => {
-    res.send('Welcome to the Amazon Scraper API');
+    res.json('Welcome to the YouTube Scraper API');
 })
 
+app.get('/test', (req, res) => {
+    //const {searchTerm} = req.params; 
+    searchTerm = "nba"
+    try{
+        axios.get("https://www.youtube.com/results?search_query=" + searchTerm, {
+            params: {
+                _limit: 1
+            }
+        })
+        .then(response => {
+            const html = response.data
+            res.json(html);
+            /*
+            const $ = cheerio.load(html)
+            $('a:contains("glue")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                results.push({ 
+                    productName: title, 
+                    url: url
+                })
+            })*/
+        })
+    } catch (e) {
+        console.log(e)
+    }
+})
 // Listen on changes at port
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
